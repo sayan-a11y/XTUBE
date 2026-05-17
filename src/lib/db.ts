8b94441b68c031461b63da97b416678faf1e3405
sayankarmakar8920-art <sayankarmakar159@gmail.com>
2026-05-17 21:58:22 +0530

    fix: change fileSize fields to BigInt to support large video uploads exceeding 2.14GB and add global BigInt serialization polyfill

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
+// Global BigInt JSON serialization polyfill
+if (typeof BigInt !== 'undefined' && !(BigInt.prototype as any).toJSON) {
+  ;(BigInt.prototype as any).toJSON = function () {
+    const num = Number(this)
+    return Number.isSafeInteger(num) ? num : this.toString()
+  }
+}
+
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
